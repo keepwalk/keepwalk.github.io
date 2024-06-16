79dce2c05e40b2118a67a7036c2ba35beaee6332
keepwalk <172935883+keepwalk@users.noreply.github.com>
2024-06-16 09:06:14 +0200

    Delete assets directory

--- assets/js/normal/tools.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.geopattern').forEach(el => {
-        var pattern = GeoPattern.generate(el.dataset.patternId).toDataUrl();
-        el.style.backgroundImage = pattern;
-    });
-});
